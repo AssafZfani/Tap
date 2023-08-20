b7a587cbc177a83052fc695d10d8d61adfbd6a30
azfani <azfani@augury.com>
2023-08-20 17:04:44 +0300

    Small fixes

--- app/src/main/java/zfani/assaf/tap/ui/MainViewModel.kt
+++ app/src/main/java/zfani/assaf/tap/ui/MainViewModel.kt
@@ -5,7 +5,6 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.Lazy
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import zfani.assaf.tap.data.remote.dto.Video
 import zfani.assaf.tap.domain.repository.VideoRepository
